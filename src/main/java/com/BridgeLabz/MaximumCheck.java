92cdff8e053f69338a4318890f121725f274411a
mouna07 <mounamc261@gmail.com>
2021-02-19 20:33:41 +0530

    ADDED: welcome Statement

---
+++ src/main/java/com/BridgeLabz/MaximumCheck.java
@@ -0,0 +1,7 @@
+package com.BridgeLabz;
+
+public class MaximumCheck {
+    public static void main(String args[]){
+        System.out.println("welcome to the maximum check problem");
+    }
+}
---
+++ src/test/java/com/BridgeLabz/MaximumCheckTest.java
@@ -0,0 +1,7 @@
+package com.BridgeLabz;
+
+public class MaximumCheckTest {
+    public static void main(String args[]){
+        System.out.println("welcome to the maximum check Test problem");
+    }
+}
