d42614b878742d5882ca8fcc1c77e980d10a5e52
mouna07 <mounamc261@gmail.com>
2021-02-19 20:57:33 +0530

    ADDED: Checking the maximum of 3 float number and testing max number by keeping in different position

--- src/test/java/com/BridgeLabz/MaximumCheckTest.java
+++ src/test/java/com/BridgeLabz/MaximumCheckTest.java
@@ -30,4 +30,26 @@ public class MaximumCheckTest {
         Integer maximumInteger = MaximumCheck.maximumInteger(4, 11, 12);
         Assert.assertSame(12, maximumInteger);
     }
+
+    /* Maximum Float element at the position 1 */
+    @Test
+    public void maximumFloatAtPosition1() {
+        Float maximumFloat = MaximumCheck.maximumFloat(12.25f, 4.249f, 12.2495f);
+        Assert.assertEquals((Float) 12.25f, maximumFloat);
+    }
+
+    /* Maximum Float element at the position 2 */
+    @Test
+    public void maximumFloatAtPosition2() {
+        Float maximumFloat = MaximumCheck.maximumFloat(12.25f, 19.99f, 6.62f);
+        Assert.assertEquals((Float) 19.99f, maximumFloat);
+    }
+
+    /* Maximum Float element at the position 3 */
+    @Test
+    public void maximumFloatAtPosition3() {
+        Float maximumFloat = MaximumCheck.maximumFloat(4.46f, 11.0f, 12.25f);
+        Assert.assertEquals((Float) 12.25f, maximumFloat);
+    }
+
 }
